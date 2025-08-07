88e201731fdcbbef9161dc6899936354af356a53
Ateeq <mdateequrrahma123@gmail.com>
2025-08-07 22:05:35 +0530

    New file added

---
+++ bitmanupulation.java
@@ -0,0 +1,9 @@
+public class bitmanupulation {
+    public static void main(String[] args) {
+        for(char ch ='a'; ch <='z'; ch++) {
+            System.out.println((char)(ch|' '));
+        }
+    }
+}
+
+
